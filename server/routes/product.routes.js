6fff762f54cc28aa52e9830cf09c2690edbebc19
Theresa Miles <ThereIsASmile@gmail.com>
2023-05-17 23:33:59 -0400

    routes notes updated

--- server/routes/product.routes.js
+++ server/routes/product.routes.js
@@ -7,11 +7,12 @@ module.exports = (app) => {
     // app is needed in the parantheses because it matches in the server.js as the server object!
     app.post('/api/product/create', ProductController.createProduct);
     app.get('/api/product/get', ProductController.getAllProducts);
-    // can be the same route when the HTTP verbs are different
     app.get('/api/product/get/:id', ProductController.getOneProduct);
     app.patch('/api/product/edit/:id', ProductController.updateProduct);
     app.delete('api/product/delete/:id', ProductController.deleteProduct); 
 }
+// route paths need names that are detailed so that the app is scalable. If there is more than one form to post or get then separate paths are needed!
+
 // These routes match the Axios call! These routes are for the API they do not match with the path for components to follow. This is just for the backend! DO not use these to view frontend page!
 
 // this function includes: HTTP verb get, request route, and what the api is to do when that route is hit.
